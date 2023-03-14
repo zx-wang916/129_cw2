254ab266a35eae4964693d7c25a7c27ec283b1f3
harry <969900528@qq.com>
2023-03-14 22:19:16 +0000

    now we can divide the dataset into labeled and unlabeled.
    I don't know why sometimes the IOU value during validation becomes NaN????
    
    Next, mean-teacher!

--- dataset.py
+++ dataset.py
@@ -1,22 +1,89 @@
 import torch
+import numpy as np
 
-from torchvision.datasets import OxfordIIITPet
+from PIL import Image
+from torchvision.datasets import OxfordIIITPet, VisionDataset
 from torchvision.transforms import transforms
 
 
 IMG_SIZE = (256, 256)
 
 
-class OxfordIIITPetSeg(OxfordIIITPet):
-    def __init__(self, root, split, download=True):
-        super().__init__(root, split, 'segmentation', download=download)
+# class OxfordIIITPetSeg(OxfordIIITPet):
+#     def __init__(self, root, split, download=True):
+#         super().__init__(root, split, 'segmentation', download=download)
+#
+#         self.multi_channel = True if split == 'trainval' else False
+#
+#         self.transform1 = transforms.Compose([transforms.PILToTensor()])
+#         self.transform2 = transforms.Compose([
+#             transforms.ToPILImage(),
+#             transforms.Resize(IMG_SIZE),
+#             transforms.ToTensor()
+#         ])
+#
+#         self.transform3 = transforms.Compose([
+#             transforms.ToPILImage(),
+#             transforms.Resize(IMG_SIZE, transforms.InterpolationMode.NEAREST),
+#             transforms.PILToTensor()
+#         ])
+#
+#     def __getitem__(self, item):
+#         data, mask = super(OxfordIIITPetSeg, self).__getitem__(item)
+#
+#         # transform to Tensor before padding
+#         data = self.transform1(data)
+#         mask = self.transform1(mask)
+#
+#         # pad the data and mask to a square image
+#         height, width = data.shape[1:]
+#         diff = abs(height - width)
+#
+#         if height > width:
+#             pad = (diff // 2, diff - diff // 2, 0, 0)
+#         else:
+#             pad = (0, 0, diff // 2, diff - diff // 2)
+#
+#         data = torch.nn.functional.pad(data, pad, mode='constant', value=0)
+#         mask = torch.nn.functional.pad(mask, pad, mode='constant', value=3)
+#
+#         # resize the image and mask, and remap the label from 123 to 012
+#         data = self.transform2(data)
+#         mask = self.transform3(mask) - 1
+#
+#         if self.multi_channel:
+#             # make the mask into multichannel
+#             mask_out = torch.zeros((3, *IMG_SIZE))
+#             mask = mask.squeeze(0)
+#             mask_out[(0, *torch.where(mask == 0))] = 1
+#             mask_out[(1, *torch.where(mask == 1))] = 1
+#             mask_out[(2, *torch.where(mask == 2))] = 1
+#         else:
+#             mask_out = mask.to(torch.float32)
+#
+#         return data, mask_out
+#
 
-        self.multi_channel = True if split == 'trainval' else False
+class OxfordIIITPetSeg1(VisionDataset):
+    def __init__(self, root, train=True, labeled_ratio=0.5):
+        super().__init__(root)
+
+        # download the dataset
+        _ = OxfordIIITPet(root, download=True)
+
+        self.train = train
+
+        self.data = None
+        self.mask = None
+        self.unlabeled = None
+
+        self.load_data_path(root, train)
+        self.divide_dataset(labeled_ratio)
 
         self.transform1 = transforms.Compose([transforms.PILToTensor()])
         self.transform2 = transforms.Compose([
             transforms.ToPILImage(),
-            transforms.Resize(IMG_SIZE),
+            transforms.Resize(IMG_SIZE, transforms.InterpolationMode.BILINEAR),
             transforms.ToTensor()
         ])
 
@@ -26,15 +93,83 @@ class OxfordIIITPetSeg(OxfordIIITPet):
             transforms.PILToTensor()
         ])
 
-    def __getitem__(self, item):
-        data, mask = super(OxfordIIITPetSeg, self).__getitem__(item)
+    def load_data_path(self, root, train):
+        self.data = []
+        self.mask = []
+
+        if train:
+            file_name = 'trainval.txt'
+        else:
+            file_name = 'test.txt'
+
+        with open(root + '/oxford-iiit-pet/annotations/' + file_name) as file:
+            for line in file:
+                image_filename, label, *_ = line.strip().split()
+                self.mask.append(root + '/oxford-iiit-pet/annotations/trimaps/' + image_filename + '.png')
+                self.data.append(root + '/oxford-iiit-pet/images/' + image_filename + '.jpg')
+
+        self.data = np.array(self.data, dtype=object)
+        self.mask = np.array(self.mask, dtype=object)
+
+    def divide_dataset(self, labeled_ratio):
+
+        # shuffle the dataset
+        idx_shuffle = np.arange(len(self))
+        np.random.shuffle(idx_shuffle)
+
+        self.data = self.data[idx_shuffle]
+        self.mask = self.mask[idx_shuffle]
+
+        # divide the dataset
+        idx_labeled = int(len(self) * labeled_ratio)
+        self.unlabeled = self.data[idx_labeled:]
+        self.data = self.data[:idx_labeled]
+        self.mask = self.mask[:idx_labeled]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # sample labeled data with index
+        data = Image.open(self.data[idx]).convert("RGB")
+        mask = Image.open(self.mask[idx]).convert("L")
 
         # transform to Tensor before padding
         data = self.transform1(data)
         mask = self.transform1(mask)
 
+        # pad the image to square image
+        data = self.pad_to_square_image(data, 0)
+        mask = self.pad_to_square_image(mask, 3)
+
+        # resize the image and mask
+        data = self.transform2(data)
+        mask = self.transform3(mask)
+
+        # make the mask into multichannel
+        mask_out = torch.zeros((3, *IMG_SIZE))
+        mask = mask.squeeze(0)
+        mask_out[(0, *torch.where(mask == 1))] = 1
+        mask_out[(1, *torch.where(mask == 2))] = 1
+        mask_out[(2, *torch.where(mask == 3))] = 1
+
+        if self.train:
+            # randomly sample unlabeled data
+            unlabeled_idx = np.random.randint(0, len(self.unlabeled))
+            unlabeled = Image.open(self.unlabeled[unlabeled_idx]).convert("RGB")
+            unlabeled = self.transform1(unlabeled)
+            unlabeled = self.pad_to_square_image(unlabeled, 0)
+            unlabeled = self.transform2(unlabeled)
+
+            return data, mask_out, unlabeled
+
+        return data, mask_out
+
+    @staticmethod
+    def pad_to_square_image(img, pad_value):
+
         # pad the data and mask to a square image
-        height, width = data.shape[1:]
+        height, width = img.shape[1:]
         diff = abs(height - width)
 
         if height > width:
@@ -42,21 +177,4 @@ class OxfordIIITPetSeg(OxfordIIITPet):
         else:
             pad = (0, 0, diff // 2, diff - diff // 2)
 
-        data = torch.nn.functional.pad(data, pad, mode='constant', value=0)
-        mask = torch.nn.functional.pad(mask, pad, mode='constant', value=3)
-
-        # resize the image and mask, and remap the label from 123 to 012
-        data = self.transform2(data)
-        mask = self.transform3(mask) - 1
-
-        if self.multi_channel:
-            # make the mask into multichannel
-            mask_out = torch.zeros((3, *IMG_SIZE))
-            mask = mask.squeeze(0)
-            mask_out[(0, *torch.where(mask == 0))] = 1
-            mask_out[(1, *torch.where(mask == 1))] = 1
-            mask_out[(2, *torch.where(mask == 2))] = 1
-        else:
-            mask_out = mask.to(torch.float32)
-
-        return data, mask_out
+        return torch.nn.functional.pad(img, pad, mode='constant', value=pad_value)
--- train_supervised.py
+++ train_supervised.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 
 from torch.utils.data import DataLoader
-from dataset import OxfordIIITPetSeg
+from dataset import OxfordIIITPetSeg1
 from model import ResUNet
 from utils import create_dir, dice_loss
 from tqdm import tqdm
@@ -12,19 +12,19 @@ create_dir()
 
 def train_supervised():
     # hyper-parameters
-    BATCH_SIZE = 128
+    BATCH_SIZE = 64
     LABELED_RATIO = 0.5
     LR = 1e-3
     EPOCH = 50
-    ALPHA = 0.5
-    DEVICE = torch.device('cuda:2')
+    DEVICE = torch.device('cuda:3')
+    # DEVICE = torch.device('cpu')
     NUM_WORKERS = 8
     PATH = '.'
 
     # preparing dataset
-    train_set = OxfordIIITPetSeg(PATH + '/data', 'trainval', download=True)
+    train_set = OxfordIIITPetSeg1(PATH + '/data', train=True, labeled_ratio=LABELED_RATIO)
     train_loader = DataLoader(train_set, BATCH_SIZE, True, num_workers=NUM_WORKERS)
-    test_set = OxfordIIITPetSeg(PATH + '/data', 'test', download=True)
+    test_set = OxfordIIITPetSeg1(PATH + '/data', train=False)
     test_loader = DataLoader(test_set, BATCH_SIZE, True, num_workers=NUM_WORKERS)
 
     # initialize network
@@ -32,7 +32,6 @@ def train_supervised():
     net = net.to(DEVICE)
 
     # define loss and optimizer
-    # criterion = torch.nn.MSELoss()
     criterion = dice_loss
     optim = torch.optim.Adam(net.parameters(), lr=LR)
 
@@ -43,18 +42,16 @@ def train_supervised():
         loss_history = []
 
         # for data, mask in train_loader:
-        for data, mask in tqdm(train_loader, desc='training progress', leave=False):
+        for data, mask, _ in tqdm(train_loader, desc='training progress', leave=False):
 
             # separate the data and mask into labeled and unlabeled parts
             data, mask = data.to(DEVICE), mask.to(DEVICE)
-            idx_labeled = int(len(data) * LABELED_RATIO)
-            data_labeled, mask_labeled = data[:idx_labeled], mask[:idx_labeled]
 
             # network predict
-            out = net(data_labeled)
+            out = net(data)
 
             # compute loss
-            loss = criterion(out, mask_labeled)
+            loss = criterion(out, mask)
 
             # backward propagation and parameter update
             optim.zero_grad()
@@ -63,7 +60,7 @@ def train_supervised():
 
             loss_history.append(loss.cpu().data.numpy())
 
-        print('epoch: %d | dice loss: %.3f' % (epoch, float(np.mean(loss_history))))
+        print('epoch: %d | train | dice loss: %.3f' % (epoch, float(np.mean(loss_history))))
 
         torch.save(net.state_dict(), PATH + '/model/supervised/net_%d.pth' % epoch)
 
@@ -79,26 +76,27 @@ def train_supervised():
 
                 # network predict
                 out = net(data)
-                out = torch.argmax(out, dim=1, keepdim=True)
+                out = torch.argmax(out, dim=1)
 
                 # compute the pixel accuracy metric
-                PA += torch.sum(out == mask)
-                PA_TOTAL += np.cumprod(mask.shape)[-1]
+                mask_pa = torch.argmax(mask, dim=1)
+                PA += torch.sum(out == mask_pa)
+                PA_TOTAL += np.cumprod(mask_pa.shape)[-1]
 
-                # compute the IOU metric
                 for i in range(3):
-                    out_class_i = torch.zeros_like(mask)
+                    # compute binary mask for segmentation of each class
+                    out_class_i = torch.zeros_like(out)
                     out_class_i[torch.where(out == i)] = 1
-                    mask_class_i = torch.zeros_like(mask)
-                    mask_class_i[torch.where(mask == i)] = 1
+                    mask_class_i = mask[:, i]
 
-                    region_intersection = torch.sum(out_class_i * mask_class_i > 0, dim=(1, 2, 3))
-                    region_union = torch.sum(out_class_i + mask_class_i > 0, dim=(1, 2, 3))
+                    # compute the IOU metric
+                    region_intersection = torch.sum(out_class_i * mask_class_i > 0, dim=(1, 2))
+                    region_union = torch.sum(out_class_i + mask_class_i > 0, dim=(1, 2))
 
                     IOU += torch.sum(region_intersection / region_union)
                     IOU_TOTAL += len(mask)
 
-        print('epoch: %d | PA: %.3f | IOU: %.3f' % (epoch, PA / PA_TOTAL, IOU / IOU_TOTAL))
+        print('epoch: %d | val | PA: %.3f | IOU: %.3f' % (epoch, PA / PA_TOTAL, IOU / IOU_TOTAL))
 
 
 if __name__ == '__main__':
